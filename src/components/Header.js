b34f6876061124a04e3f6d7fee1db8abdb600a92
jolentino <johnm.tolentino@gmail.com>
2021-01-15 22:20:23 -0500

    Added logo, navigation, and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import logo from '../imgs/leaf.svg';
+
 const Header = () => {
 	return (
-		<div>
-			<Link to="/">Logo</Link>
-			<div>Pick'd</div>
-			<Link>Cart</Link>
+		<div className="ui secondary pointing menu">
+			<Link to="/" className="link item">
+				<img
+					src={logo}
+					style={{
+						width: '20px',
+						marginRight: '5px',
+					}}
+					alt="Pick'd Logo"
+				/>
+				Pick'd
+			</Link>
+			<div className="right menu">
+				<Link to="/cart" className="link item">
+					Cart
+				</Link>
+			</div>
 		</div>
 	);
 };
