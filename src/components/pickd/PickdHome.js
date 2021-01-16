85537990f453b364c4986dc2b8a25bb4fd432c08
jolentino <johnm.tolentino@gmail.com>
2021-01-15 22:28:58 -0500

    Pages created

--- src/components/pickd/PickdHome.js
+++ src/components/pickd/PickdHome.js
@@ -1,9 +1,26 @@
 import * as React from 'react';
 
-class PickdHome extends React.Component {
-  render() {
-    return <div>Pick'd Home</div>
-  }
-}
+import history from '../../history';
+import Button from '../utilities/Button';
+import logo from '../../imgs/leaf.svg';
+
+const PickdHome = () => {
+	const label = 'Build Your Protein';
+
+	const onButtonClick = () => {
+		history.push('/create-a-product');
+	};
+
+	return (
+		<div className="ui vertical center aligned segment">
+			<img src={logo} alt="Pick'd Logo" style={{ width: '100px' }} />
+			<h2>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam fugit, voluptate aliquid vitae accusamus corrupti non, tempore magni consequatur
+				doloremque amet, itaque molestiae ipsa quisquam blanditiis assumenda dignissimos? Maxime, id.
+			</h2>
+			<Button buttonConfig={{ label, onButtonClick }} className="item" />
+		</div>
+	);
+};
 
 export default PickdHome;
--- src/components/pickd/PickdProduct.js
+++ src/components/pickd/PickdProduct.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 
 class PickdProduct extends React.Component {
-  render() {
-    return <div>Pick'd Product</div>
-  }
+	render() {
+		return <div>Pick'd Product</div>;
+	}
 }
 
 export default PickdProduct;
