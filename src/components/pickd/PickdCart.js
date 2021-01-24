a965b524f43e773f099866aff8f0f92eb317a64b
jolentino <johnm.tolentino@gmail.com>
2021-01-23 23:13:01 -0500

    Order summary display

--- src/components/pickd/PickdCart.js
+++ src/components/pickd/PickdCart.js
@@ -1,9 +1,39 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+
+import { textConverter } from '../../textConverter';
 
 class PickdCart extends React.Component {
+	renderProductSummary() {
+		// Array for choices descriptions.
+		const order = [];
+		const { product } = this.props;
+
+		for (let choice in product) {
+			order.push(
+				<div>
+					<p>
+						<span style={{ fontWeight: 'bold' }}>{textConverter(choice)}:</span> {textConverter(product[choice])}
+					</p>
+				</div>
+			);
+		}
+
+		return order;
+	}
+
 	render() {
-		return <div>Pick'd Cart</div>;
+		return (
+			<div className="ui raised container segment center aligned">
+				<h1>Your Order:</h1>
+				<div>{this.props.product ? this.renderProductSummary() : 'Your cart is empty!'}</div>
+			</div>
+		);
 	}
 }
 
-export default PickdCart;
+const mapStateToProps = (state) => {
+	return { product: state.pickd.product };
+};
+
+export default connect(mapStateToProps)(PickdCart);
