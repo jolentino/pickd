f5407a8a4410dbcab8b76d26b67fefb832434d44
jolentino <johnm.tolentino@gmail.com>
2021-01-24 21:02:40 -0500

    Added actions for cart

--- src/components/pickd/PickdCart.js
+++ src/components/pickd/PickdCart.js
@@ -2,8 +2,11 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import { textConverter } from '../../textConverter';
+import Button from '../utilities/Button';
+import history from '../../history';
 
 class PickdCart extends React.Component {
+	// Helper method to render order summary.
 	renderProductSummary() {
 		// Array for choices descriptions.
 		const order = [];
@@ -22,11 +25,31 @@ class PickdCart extends React.Component {
 		return order;
 	}
 
+	// Helper method for cart buttons.
+	configureButton = (label, location, color) => {
+		return (
+			<Button
+				buttonConfig={{
+					label: label,
+					onButtonClick: () => {
+						history.push(`/${location}`);
+					},
+					color: color,
+				}}
+			/>
+		);
+	};
+
+	// Conditional cart.
 	render() {
 		return (
 			<div className="ui raised container segment center aligned">
 				<h1>Your Order:</h1>
 				<div>{this.props.product ? this.renderProductSummary() : 'Your cart is empty!'}</div>
+				<div>
+					{this.props.product ? this.configureButton('Start Over', 'create-a-product', 'grey') : null}
+					{this.props.product ? this.configureButton('Complete Order', 'order') : null}
+				</div>
 			</div>
 		);
 	}
