0a1af16c57d328db32af37f6055d718f0da499aa
jolentino <johnm.tolentino@gmail.com>
2021-01-29 15:43:17 -0500

    Confirmation template

--- src/components/pickd/PickdConfirmation.js
+++ src/components/pickd/PickdConfirmation.js
@@ -1,9 +1,28 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 
 class PickdConfirmation extends React.Component {
 	render() {
-		return <div>Pick'd Confirmation</div>;
+		const { payer, purchase_units } = this.props.confirmation;
+		return (
+			<div className="ui raised container segment center aligned">
+				<h1>Order Confirmation:</h1>
+				<h4>Order ID: {this.props.confirmation.id}</h4>
+				<p>
+					Thank you <span>{`${payer.name.given_name} ${payer.name.surname}`}</span> for your order! We've received your order and will contact you as soon as
+					your package is shipped.
+				</p>
+				<p>
+					An email with additional details about your order was sent to <span>{purchase_units[0].payee.email_address}</span>
+				</p>
+				<h3>Thank you for picking Pick'd!</h3>
+			</div>
+		);
 	}
 }
 
-export default PickdConfirmation;
+const mapStateToProps = (state) => {
+	return { confirmation: state.pickd.confirmation };
+};
+
+export default connect(mapStateToProps)(PickdConfirmation);
