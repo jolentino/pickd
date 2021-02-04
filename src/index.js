c1025ba292f632382af30467c9ba50596316a0c5
jolentino <johnm.tolentino@gmail.com>
2021-02-04 11:22:31 -0500

    Removed redux dev tool + comments

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import {PayPalScriptProvider} from '@paypal/react-paypal-js';
 import App from './components/App';
 import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const clientId = 'ARBS1mRGAzRgrWyrSSlDhknsGz0aWbpDMxkV9Xo7GFCWQxd3ZPMz7Bx9A0tBv3ePPTIPtYJItPB2yCtE';
 
 ReactDOM.render(
-	<Provider store={createStore(reducers, composeEnhancers(applyMiddleware(thunk)))}>
+	<Provider store={createStore(reducers, applyMiddleware(thunk))}>
+		{/* Provider to access PayPal Button */}
 		<PayPalScriptProvider options={{ 'client-id': clientId }}>
 			<App />
 		</PayPalScriptProvider>
